4c3c6b36d6599342059809a46e4d452d07704619
hutao <1052661752@qq.com>
2021-12-19 17:59:12 +0800

    构建maven测试

---
+++ mavenTest/src/main/java/com/maven/test/Maven.java
@@ -0,0 +1,33 @@
+package com.maven.test;
+
+/**
+ * @Classname Maven
+ * @Description TODO
+ * @Date 2021/11/25 下午10:12
+ * @Created by mac
+ */
+public class Maven {
+    public String hutao ;
+    public int age ;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHutao(String hutao) {
+        this.hutao = hutao;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHutao() {
+        return hutao;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ mavenTest/src/main/java/com/maven/test/MavenMain.java
@@ -0,0 +1,17 @@
+package com.maven.test;
+
+/**
+ * @Classname com.maven.test.MavenMain
+ * @Description TODO
+ * @Date 2021/11/25 下午10:02
+ * @Created by mac
+ */
+public class MavenMain {
+    public static void main(String[] args  ){
+        //while (true){
+        //    System.out.println("info :.......... " );
+        //}
+
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
